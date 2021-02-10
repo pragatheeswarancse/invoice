93adeed7b2e7af21d1df6c9ea296de65215b4581
pragatheeswarancse <kasipragi143@gmailcom>
2021-02-10 21:04:18 +0530

    location add in existing company

--- components/employee/existingCompany.js
+++ components/employee/existingCompany.js
@@ -16,7 +16,7 @@ export default function ExistingCompany() {
                                 </th>
                                 <th scope="col"
                                     className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Title
+                                    Location
                                 </th>
                                 <th scope="col"
                                     className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
